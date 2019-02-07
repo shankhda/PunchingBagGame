68ed63edb1b9135bf24f631348cd71f1401fdd98
Deeksha Shankhdher <shankhda@mail.uc.edu>
2019-02-07 02:11:38 -0500

    Created Game skeleton

---
+++ PunchingBag.js
@@ -0,0 +1,21 @@
+new Vue ({
+
+    el:'punching-bag-app',
+    data:{
+        health:100,
+        gameEnd:false
+    },
+    methods:{
+        punch:function(){
+            this.health-=10;
+            if(this.health<=0)
+            {
+                this.gameEnd=true;
+            }
+        },
+        restart:function(){
+            this.health = 100;
+        }
+    }
+
+});
